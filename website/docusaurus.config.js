4bf52e94f9e83423f87703fe342195d799415462
Rafael Henrique Capati <rhcapati@gmail.com>
2025-03-07 09:44:22 -0300

    website: update tagline

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,7 +11,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
 	title: 'Odin Vulkan Guide',
-	tagline: 'Dinosaurs are cool',
+	tagline: 'Practical guide to Vulkan graphics programming using Odin Language',
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
