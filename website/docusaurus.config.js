156b7b30a4602edb1ffd971252fb862f87c18d68
Rafael Henrique Capati <rhcapati@gmail.com>
2025-03-09 11:46:09 -0300

    fix: update edit URL in docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						'https://github.com/Capati/odin-vk-guide/website/',
+						'https://github.com/Capati/odin-vk-guide/edit/main/website',
 				},
 				blog: false,
 				theme: {
